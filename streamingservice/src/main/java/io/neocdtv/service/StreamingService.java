024237f950156c39c5f4bbd7b59a1d1836c1b9c3
neocdtv <krzysztof.wolf.1980@gmail.com>
2018-05-23 09:33:47 +0200

    enable multi-homed

--- streamingservice/src/main/java/io/neocdtv/service/StreamingService.java
+++ streamingservice/src/main/java/io/neocdtv/service/StreamingService.java
@@ -4,27 +4,34 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.SocketAddress;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class StreamingService {
   private final static Logger LOGGER = Logger.getLogger(StreamingService.class.getName());
-  static int PORT;
+  //TODO: SocketAddress check if this can used to contain address and port or if maybe if will occupy the port which is required for jetty server
+  public static Map<String, Integer> addressToPort = new HashMap<>();
 
-  public static void main(String[] args) throws Exception {
-    StreamingService.startIt();
-  }
-
-  public static void startIt() throws Exception {
-    PORT = discoverFreeNetworkPort();
-    Server server = new Server(PORT);
-
-    ServletHandler handler = new ServletHandler();
-    server.setHandler(handler);
-    handler.addServletWithMapping(StreamingServlet.class, StreamingServiceConstants.SERVLET_PATH);
-    server.start();
-    LOGGER.log(Level.INFO, "starting streaming service on port {0}", PORT);
+  public static void startIt(final List<InetAddress> addresses) throws Exception {
+    for (InetAddress address : addresses) {
+      int port = discoverFreeNetworkPort();
+      Server server = new Server(port);
+      ServletHandler handler = new ServletHandler();
+      server.setHandler(handler);
+      handler.addServletWithMapping(StreamingServlet.class, StreamingServiceConstants.SERVLET_PATH);
+      server.start();
+      addressToPort.put(address.getHostAddress(), port);
+      LOGGER.log(Level.INFO,
+          String.format("starting streaming service on address %s and port %s",
+              address.getHostAddress(),
+              port));
+    }
   }
 
   private static int discoverFreeNetworkPort() throws IOException {
@@ -34,5 +41,4 @@ public class StreamingService {
     socket.close();
     return port;
   }
-
 }
--- streamingservice/src/main/java/io/neocdtv/service/UrlBuilder.java
+++ streamingservice/src/main/java/io/neocdtv/service/UrlBuilder.java
@@ -11,6 +11,7 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.URLEncoder;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,25 +21,17 @@ import java.util.logging.Logger;
  */
 public class UrlBuilder {
 
-  public static void main(String[] args) throws UnknownHostException {
-    System.out.println(build("file"));
-  }
-
-  // TODO: check if finding a ipv4 can be simplified, what about more than one interfaces, which is active and non-loopback?
   private final static Logger LOGGER = Logger.getLogger(UrlBuilder.class.getName());
-  private static final String IPV4_REGEX = "\\A(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)(\\.(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)){3}\\z";
-  private static String ipV4;
-  private static final String UTF8 = "UTF-8";
   private static boolean ENCODE_URL_PARAM = true;
 
-  public static String build(final String url) {
+  public static String build(final String url, final InetAddress address) {
     if (url.startsWith("http")) {
       return url;
     } else {
       String preparedParam = null;
       try {
         if (ENCODE_URL_PARAM) {
-          preparedParam = URLEncoder.encode(url, UTF8);
+          preparedParam = URLEncoder.encode(url, StandardCharsets.UTF_8.name());
           LOGGER.info("Encoded param: " + preparedParam);
         } else {
           preparedParam = url;
@@ -46,8 +39,8 @@ public class UrlBuilder {
       } catch (UnsupportedEncodingException ex) {
         LOGGER.log(Level.SEVERE, null, ex);
       }
-      String preparedUrl = "http://" + getIpV4Address() + ":"
-          + StreamingService.PORT
+      String preparedUrl = "http://" + address.getHostAddress() + ":"
+          + getPortForAddress(address)
           + StreamingServiceConstants.SERVLET_PATH + "?"
           + StreamingServiceConstants.PARAM_NAME_RESOURCE + "=" + preparedParam;
       LOGGER.info("Prepared Url: " + preparedUrl);
@@ -55,31 +48,7 @@ public class UrlBuilder {
     }
   }
 
-  public static String getIpV4Address() {
-    if (ipV4 == null) {
-      try {
-        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-        while (interfaces.hasMoreElements()) {
-          NetworkInterface iface = interfaces.nextElement();
-          // filters out 127.0.0.1 and inactive interfaces
-          if (iface.isLoopback() || !iface.isUp()) {
-            continue;
-          }
-
-          Enumeration<InetAddress> addresses = iface.getInetAddresses();
-          while (addresses.hasMoreElements()) {
-            InetAddress inetAddress = addresses.nextElement();
-            String currentAddress = inetAddress.getHostAddress();
-            if (currentAddress.matches(IPV4_REGEX)) {
-              System.out.println(currentAddress);
-              ipV4 = currentAddress;
-            }
-          }
-        }
-      } catch (SocketException e) {
-        throw new RuntimeException(e);
-      }
-    }
-    return ipV4;
+  private static Integer getPortForAddress(final InetAddress address) {
+    return StreamingService.addressToPort.get(address.getHostAddress());
   }
 }
